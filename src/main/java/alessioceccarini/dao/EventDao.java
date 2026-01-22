3ec0076f4779b396e39b2233fd17d6e19646cdf3
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-01-22 17:08:50 +0100

    adding_table

--- src/main/java/alessioceccarini/dao/EventDao.java
+++ src/main/java/alessioceccarini/dao/EventDao.java
@@ -1,11 +1,9 @@
 package alessioceccarini.dao;
 
-import alessioceccarini.NotFoundException;
 import alessioceccarini.entities.Event;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
-import java.util.UUID;
 
 public class EventDao {
 	private final EntityManager em;
@@ -24,12 +22,5 @@ public class EventDao {
 
 		System.out.println(event.getName() + " è stato salvato!");
 	}
-
-	public Event findById(String eventId) {
-		Event found = em.find(Event.class, UUID.fromString(eventId));
-		if (found == null) throw new NotFoundException(eventId);
-		return found;
-	}
-
-
+	
 }
--- src/main/java/alessioceccarini/entities/Event.java
+++ src/main/java/alessioceccarini/entities/Event.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "events")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "event_category")
 public abstract class Event {
 	@Id
